3e4447e7b57f95a2479d25b6fa5ddf4aa20f7be3
shivani <shivaniw69@gmail.com>
2025-07-28 18:43:36 +0530

    script2

--- script2.js
+++ script2.js
@@ -1,30 +1,5 @@
 
 
-// let chatList = document.querySelector('#chat-list');
-// let messageInput = document.querySelector('#message-input');
-// let sendButton = document.querySelector('#send-button');
-
-// let messages = [];
-
-// sendButton.addEventListener('click', function() {
-//   let message = messageInput.value.trim();
-//   if (message !== '') {
-//     messages.push(message);
-//     displayMessages();
-//     messageInput.value = '';
-//   }
-// });
-
-// let displayMessages=()=> {
-//   chatList.innerHTML = '';
-//   messages.forEach(function(message, index) {
-//     let li = document.createElement('li');
-//     li.textContent = message;
-//     chatList.appendChild(li);
-//   });
-// }
-
-
 let chatList = document.querySelector('#chat-list');
 let messageInput = document.querySelector('#message-input');
 let sendButton = document.querySelector('#send-button');
