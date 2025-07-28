6ea654002ef56c066b348cdf8e035fd58194cae3
shivani <shivaniw69@gmail.com>
2025-07-28 18:42:52 +0530

    script

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-    // let sendMessage=()=> {
-    //     let input = document.querySelector("#messageInput");
-    //     let chatBox = document.querySelector("#chatBox");
-
-    //     if (input.value.trim() !== "") {
-    //         let message = document.createElement("div");
-    //         message.classList.add("message", "user-message");
-    //         message.textContent = input.value;
-    //         chatBox.appendChild(message);
-    //         chatBox.scrollTop = chatBox.scrollHeight;
-    //         input.value = "";
-    //     }
-    // }
-
-
     let sendMessage=()=> {
         let input = document.querySelector("#messageInput");
         let chatBox = document.querySelector("#chatBox");
